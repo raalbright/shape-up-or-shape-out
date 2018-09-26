f5f09029c5cd9cb0069971c4b0684e8208fb34b1
raalbright <ray.a.albright@gmail.com>
2018-09-26 13:53:15 -0500

    can add squares to page

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const canvas = document.querySelector('#canvas');
+
 class Shape {
     constructor(shapeName) {
 
@@ -30,5 +32,25 @@ class Square extends Shape {
     constructor(sideLength) {
         super('square');
         this.sideLength = sideLength;
+
+        this.element = document.createElement('div');
+        this.element.classList.add('square');
+        this.element.style.width = `${sideLength}px`;
+        this.element.style.height = `${sideLength}px`;
+        this.element.style.position = 'absolute';
+        this.element.style.top = `${Math.abs(Math.floor(Math.random() * 600) - sideLength)}px`;
+        this.element.style.right = `${Math.abs(Math.floor(Math.random() * canvas.offsetWidth) - sideLength)}px`;
+
+        canvas.appendChild(this.element);
     }
 }
+
+const squareForm = document.querySelector('#add-square');
+
+squareForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const sideLength = parseInt(form.elements["side-length"].value);
+    new Square(sideLength);
+    // form.reset();
+});
