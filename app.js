31cf94dc83cd8c9a8cd422f399929fa1bea0392d
raalbright <ray.a.albright@gmail.com>
2018-09-26 15:08:27 -0500

    can add triangles to page

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ class Triangle extends Shape {
     constructor(height) {
         super('triangle');
         this.height = height;
+
+        this.element = document.createElement('div');
+        this.element.classList.add('triangle');
+        this.element.style.borderTop = `${height}px solid yellow`;
+        this.element.style.borderRight = `${height}px solid transparent`;
+        this.element.style.position = 'absolute';
+        this.element.style.top = `${Math.abs(Math.floor(Math.random() * 600) - height)}px`;
+        this.element.style.right = `${Math.abs(Math.floor(Math.random() * canvas.offsetWidth) - height)}px`;
+
+        canvas.appendChild(this.element);
     }
 }
 
@@ -68,6 +78,7 @@ class Square extends Shape {
 const squareForm = document.querySelector('#add-square');
 const circleForm = document.querySelector('#add-circle');
 const rectangleForm = document.querySelector('#add-rectangle');
+const triangleForm = document.querySelector('#add-triangle');
 
 squareForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -91,5 +102,13 @@ rectangleForm.addEventListener('submit', (e) => {
     const width = parseInt(form.elements["width"].value);
     const height = parseInt(form.elements["height"].value);
     new Rectangle(width, height);
+    // form.reset();
+});
+
+triangleForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const height = parseInt(form.elements["height"].value);
+    new Triangle(height);
     // form.reset();
 });
